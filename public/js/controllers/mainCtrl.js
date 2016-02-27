2fd87b260ffbc5f9859d2955aa08282d27fce100
calimanleontin <calimanleontin@gmail.com>
2016-02-27 23:04:06 +0200

    remade the angular

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,62 +1,41 @@
 angular.module('mainCtrl', [])
 
-    // inject the Article service into our controller
-    .controller('mainController', function($scope, $http, Article) {
-        // object to hold all the data for the new Article form
+    .controller('mainController', function($scope, $http, Article){
+
         $scope.articleData = {};
 
-        // loading variable to show the spinning loading icon
         $scope.loading = true;
 
-        // get all the Articles first and bind it to the $scope.Articles object
-        // use the function we created in our service
-        // GET ALL ArticleS ==============
         Article.get()
-            .success(function(data) {
+            .success(function(data){
                 $scope.articles = data;
                 $scope.loading = false;
             });
 
-        // function to handle submitting the form
-        // SAVE A Article ================
-        $scope.submitArticle = function() {
+
+        $scope.submitArticle = function(){
             $scope.loading = true;
 
-            // save the Article. pass in Article data from the form
-            // use the function we created in our service
             Article.save($scope.articleData)
-                .success(function(data) {
-
-                    // if successful, we'll need to refresh the Article list
+                .success(function(data){
                     Article.get()
-                        .success(function(getData) {
+                        .success(function(getData){
                             $scope.articles = getData;
                             $scope.loading = false;
                         });
-
-                })
-                .error(function(data) {
-                    console.log(data);
                 });
-        };
+        }
 
-        // function to handle deleting a Article
-        // DELETE A Article ====================================================
-        $scope.deleteArticle = function(id) {
+        $scope.deleteArticle = function(id){
             $scope.loading = true;
 
-            // use the function we created in our service
             Article.destroy(id)
-                .success(function(data) {
-
-                    // if successful, we'll need to refresh the Article list
+                .success(function(data){
                     Article.get()
-                        .success(function(getData) {
+                        .success(function(getData){
                             $scope.articles = getData;
                             $scope.loading = false;
                         });
-
                 });
-        };
-
+        }
     });
--- public/js/services/articleService.js
+++ public/js/services/articleService.js
@@ -1,27 +1,24 @@
 angular.module('articleService', [])
 
-    .factory('Article', function($http) {
+    .factory('Article', function($http){
+        return{
 
-        return {
-            // get all the articles
-            get : function() {
+            get : function(){
                 return $http.get('/api/articles');
             },
 
-            // save a article (pass in article data)
             save : function(articleData) {
                 return $http({
-                    method: 'POST',
-                    url: '/api/articles',
-                    headers: { 'Content-Type' : 'application/x-www-form-urlencoded' },
-                    data: $.param(articleData)
+                    method  :   'POST',
+                    url     :   '/api/articles',
+                    data    : $.param(articleData),
+                    headers : {'Content-Type' : 'application/x-www-form-urlencoded'}
+
                 });
             },
 
-            // destroy a article
             destroy : function(id) {
                 return $http.delete('/api/articles/' + id);
             }
         }
-
     });

84cc9617620a91e53744feada6474abb7eb6d10c
calimanleontin <calimanleontin@gmail.com>
2016-02-27 16:47:48 +0200

    laravel article controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use \Response;
+use \Input;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    /**
+     * Send back all comments as JSON
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return Response::json(Comment::get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        Article::create(array(
+            'author' => Input::get('author'),
+            'text' => Input::get('text')
+        ));
+
+        return Response::json(array('success' => true));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Article::destroy($id);
+
+        return Response::json(array('success' => true));
+    }
+}
